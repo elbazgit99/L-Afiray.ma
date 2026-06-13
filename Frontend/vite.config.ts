a2a7e44b77da2122837f9ad88444ec6e83b94bfe
elbazgit99 <helbaz124@gmail.com>
2026-06-13 14:23:01 +0100

    fix-deployment-issues

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,15 +1,14 @@
-import path from "path";
+import { fileURLToPath, URL } from "node:url"; 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: process.env.VITE_BASE_PATH || "/L-Afiray.ma",
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-});
+});   
